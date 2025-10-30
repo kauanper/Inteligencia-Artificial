b6a1766ad5012de972000dace189c621f9f7574c
kauan <kauancavalcante8p@gmail.com>
2025-10-30 09:22:17 -0300

    criando nó

---
+++ AgenteDeProblmeas/src/No.java
@@ -0,0 +1,18 @@
+public class No implements Comparable<No> {
+    public Estado estado;
+    public No pai;
+    public Transicao acao;
+    public int custoCaminho;
+
+    public No(Estado estado, No pai, Transicao acao, int custoCaminho) {
+        this.estado = estado;
+        this.pai = pai;
+        this.acao = acao;
+        this.custoCaminho = custoCaminho;
+    }
+
+    @Override
+    public int compareTo(No outro) {
+        return Integer.compare(this.custoCaminho, outro.custoCaminho);
+    }
+}
